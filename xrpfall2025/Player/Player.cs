f98e6f3c8cf0ae3268a57d764c736d04b13e5673
Jamie Zheng <112339308+jamiezheng8653@users.noreply.github.com>
2025-09-11 14:56:22 -0400

    States Regular, Fast, Slow, and Inverted have been added. Thinking about adding friction, however we will wait till we get a robot to test on to see how we will translate movement.

--- xrpfall2025/Player/Player.cs
+++ xrpfall2025/Player/Player.cs
@@ -1,20 +1,51 @@
+//Code from https://docs.godotengine.org/en/4.4/getting_started/first_3d_game/03.player_movement_code.html
 using Godot;
+
+//Enum States
+public enum States
+{
+	Regular,
+	Inverted,
+	Slow,
+	Fast
+};
 
 public partial class Player : CharacterBody3D
 {
+	//current state
+	private States current = States.Regular;
+	//if the player gets a speedup or speed debuff, 
+	//multiply/divide _targetVelocity by cooresponding multiplier
+	//has to be int because Vectors are made up by ints
+	[Export]
+	private int slowMultiplier = 2;
+
+	[Export]
+	private int fastMultiplier = 2;
+
+	private Vector3 _targetVelocity = Vector3.Zero;
+
 	// How fast the player moves in meters per second.
 	[Export]
 	public int Speed { get; set; } = 14;
+
 	// The downward acceleration when in the air, in meters per second squared.
 	[Export]
 	public int FallAcceleration { get; set; } = 75;
-	
-	private Vector3 _targetVelocity = Vector3.Zero;
+
+	[Export]
+	//change of state. may update this when we hash out 
+	// how we want to track player state upon item collision
+	public States Current
+	{
+		get { return current; }
+		set { current = value; }
+	}
 
 	public override void _PhysicsProcess(double delta)
 	{
-		var direction = Vector3.Zero;
-		
+		Vector3 direction = Vector3.Zero;
+
 		if (Input.IsActionPressed("right"))
 		{
 			direction.X += 1.0f;
@@ -52,6 +83,28 @@ public partial class Player : CharacterBody3D
 		{
 			_targetVelocity.Y -= FallAcceleration * (float)delta;
 			//GD.Print("I'm falling!");
+		}
+
+		//check states
+		switch (current)
+		{
+			//multiply speed
+			case States.Fast:
+				_targetVelocity.X *= fastMultiplier;
+				_targetVelocity.Z *= fastMultiplier;
+				break;
+			//divide speed
+			case States.Slow:
+				_targetVelocity.X /= slowMultiplier;
+				_targetVelocity.Z /= slowMultiplier;
+				break;
+			//flip flop controls
+			case States.Inverted:
+				_targetVelocity.X *= -1;
+				_targetVelocity.Z *= -1;
+				break;
+
+			//regular has no change
 		}
 
 		// Moving the character
