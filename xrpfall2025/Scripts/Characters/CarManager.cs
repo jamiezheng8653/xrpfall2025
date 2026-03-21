45e03926168d67ac716da714397a2ae923f74f96
Jamie Zheng <jamiezheng8653@gmail.com>
2026-03-21 00:24:22 -0400

    Final testing of new placement sorting algorithm. ended up scrapping old sorting code for a merge sort.

--- xrpfall2025/Scripts/Characters/CarManager.cs
+++ xrpfall2025/Scripts/Characters/CarManager.cs
@@ -28,9 +28,16 @@ public partial class CarManager : Node
 	}
 
     public override void _Process(double delta)
-    {
-        PlacementTracker();
-    }
+	{
+		//sort the cars into their proper placements and save this new list
+		cars = MergeSort(Cars);
+
+		//adjust the placements of each car
+		for (int i = 0; i < cars.Count; i++)
+		{
+			cars[i].PlacementChanged(i + 1);
+		}
+	}
 
 	/// <summary>
 	/// Initializes all cars for the new track
@@ -61,60 +68,113 @@ public partial class CarManager : Node
 	}
 
 	/// <summary>
-	/// Calculates and sorts the placement of all cars
-	/// Admitted the current implementation can be optimized significantly.
-	/// But for now its performance is good enough for our intents and purposes.
-	/// 
+	/// Returns whether car a is infront of car b
 	/// </summary>
-	private void PlacementTracker()
+	private bool CompareCars(Car a, Car b)
 	{
-		// LinkedList<Car> ordered = new LinkedList<Car>();
-		// ordered.AddFirst(cars[0]);
-		// LinkedListNode<Car> current = ordered.First;
-		// //order the cars in terms of placement
-		// for(int i = 0; i < TOTALCARS; i++)
-		// {
-		// 	for (int j = i + 1; j < TOTALCARS; j++)
-		// 	{
-		// 		//first check if two cars are on different laps
-		// 		if (current.Value.Lap < cars[j].Lap)
-		// 		{
-		// 			current.
-		// 			ordered.AddFirst(cars[j]);
-		// 			current = cars[j];
-		// 			continue;
-		// 		}
-		// 		//if two cars are on the same lap, check who has more checkpoints
-		// 		else if (current.NumPassedCheckpoints < cars[i].Lap)
-		// 		{
-		// 			ordered.AddFirst(cars[j]);
-		// 			current = cars[j];
-		// 			continue;
-		// 		}
-		// 		//if two cars have the same amount of checkpoints, calculate who is farther from the last checkpoint passed
-		// 		else if(current.DistanceFromLastCheckpoint() < cars[i].DistanceFromLastCheckpoint())
-		// 		{
-		// 			ordered.AddFirst(cars[j]);
-		// 			current = cars[j];
-		// 			continue;
-		// 		}
-		// 		else
-		// 		{
-		// 			ordered.
-		// 		}
-		// 	}
+		//return the car that is infront of the other car
+		//if car a has passed less laps than car b, return b
+		if(a.Lap < b.Lap) return false;
+		//if car a and b are on the same lap, 
+		// then if car a has passed less checkpoints than car b, return b
+		else if (a.NumPassedCheckpoints < b.NumPassedCheckpoints) return false;
+		//if car a and b are on the same lap and have passed the 
+		// same number of checkpoints, then if car a is closer 
+		// to the last checkpoint than car b, return b
+		else if (a.DistanceFromLastCheckpoint() < b.DistanceFromLastCheckpoint()) return false;
+		//otherwise car a is infront of car b
+		else return true;
+	}
 
+	/// <summary>
+	/// Given two sorted list, sort the elements 
+	/// sequencially and return a single sorted list
+	/// </summary>
+	private List<Car> Merge(List<Car> leftList, List<Car> rightList)
+	{
+		List<Car> sorted = new List<Car>();
+		int lI = 0; //left list index
+		int rI = 0;	//right list index 
 
-		// }
+		//index through both lists and add them to each list in sequencial order
+		while (lI < leftList.Count && rI < rightList.Count)
+		{
+			// if the current leftList car is infront of the current 
+			// rightList car, then add it to the sorted list and increment lI
+			if(CompareCars(leftList[lI], rightList[rI]))
+			{
+				sorted.Add(leftList[lI]);
+				lI++;
+			}
+			//otherwise add the rightList car to the list and increment rI
+			else
+			{
+				sorted.Add(rightList[rI]);
+				rI++;
+			}
+		}
 
+		//add any remaining cars. Assumes at least either leftList or 
+		// rightList cars have been completely added to the sorted list
+		while (lI < leftList.Count)
+		{
+			sorted.Add(leftList[lI]);
+			lI++;
+		}
+		while(rI < rightList.Count)
+		{
+			sorted.Add(rightList[rI]);
+			rI++;
+		}
 
-		// //modify placement values for each car
-		// for(int i = 1; i <= TOTALCARS; i++)
-		// {
-		// 	now.Value.PlacementChanged(i);
-		// 	if(i + 1 > TOTALCARS) return;
-		// 	now = now.Next;
-		// }
+		return sorted;
+	}
 
+	/// <summary>
+	/// Recursively sorts a given list of 
+	/// cars in the order of their placement
+	/// </summary>
+	/// <param name="carList"></param>
+	private List<Car> MergeSort(List<Car> carList)
+	{
+		//as long as the size of the list is greater than one, then we can keep splitting
+		if (1 < carList.Count)
+		{
+			List<Car> leftList = new List<Car>();
+			List<Car> rightList = new List<Car>();
+			int lSize, rSize;
+
+			//divide the list recursively into two halves until it can no longer be
+			if (carList.Count % 2 == 0) 
+			{ 
+				lSize = carList.Count/2;
+				rSize = carList.Count/2;
+			}
+			else
+			{
+				lSize = carList.Count/2 - 1;
+				rSize = carList.Count/2;
+			}
+
+			//create the left list
+			for(int i = 0; i < lSize; i++)
+			{
+				leftList.Add(carList[i]);
+			}
+			//create the right list
+			for(int i = rSize; i < carList.Count; i++)
+			{
+				rightList.Add(carList[i]);
+			}
+
+			//sort the first and second halves
+			MergeSort(leftList);
+			MergeSort(rightList);
+
+			//merge the two lists and return its sorted version
+			return Merge(leftList, rightList);
+		}
+		else return null;
+		
 	}
 }
